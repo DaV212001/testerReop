10a29884f99936d5d74dfb3d62b3eea850156da4
Dagim Tesfaye Mebrate <dagimcodes@gmail.com>
2024-02-21 18:48:36 +0300

    feat: custom button

--- lib/util/app_routes.dart
+++ lib/util/app_routes.dart
@@ -1,13 +1,14 @@
 import 'package:get/get.dart';
+import 'package:mss_e_learning/screen/home/home_screen.dart';
 import 'package:mss_e_learning/screen/main_layout_screen.dart';
-
-import '../screen/home/home_screen.dart';
+import 'package:mss_e_learning/screen/onboarding/onboarding_screen.dart';
 
 class AppRoutes {
   AppRoutes._();
 
   static const initial = '/main_screen';
   static const home = '/home';
+  static const onboarding = '/onboarding';
 
   static final pages = [
     GetPage(
@@ -17,6 +18,10 @@ class AppRoutes {
     GetPage(
       name: home,
       page: () => const HomeScreen(),
+    ),
+    GetPage(
+        name: onboarding,
+        page: () => const OnboardingScreen()
     ),
   ];
 }
---
+++ lib/widget/button.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final VoidCallback onPress;
+  final bool awaitResponse;
+  final bool enabled;
+  final double? width;
+
+  const Button(
+      {super.key,
+      required this.text,
+      required this.onPress,
+      this.enabled = true,
+      this.awaitResponse = false,
+      this.width = double.infinity});
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return SizedBox(
+      width: width,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: enabled
+              ? theme.buttonTheme.colorScheme?.primary
+              : theme.buttonTheme.colorScheme?.primaryContainer,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+                10), // Set your desired border radius here
+          ),
+        ),
+        onPressed: awaitResponse
+            ? null
+            : enabled
+                ? onPress
+                : null,
+        child: awaitResponse
+            ? SizedBox(
+                width: 20.0,
+                height: 20.0,
+                child: CircularProgressIndicator(
+                    color: theme.buttonTheme.colorScheme?.onPrimary,
+                    strokeWidth: 2.0))
+            : Text(
+                text,
+                style: TextStyle(
+                    color: theme.buttonTheme.colorScheme?.onPrimary,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500),
+              ),
+      ),
+    );
+  }
+}
