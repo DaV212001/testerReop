c715ad7b8292ea637a4eddbb78e7a526512feda5
Dagim Tesfaye Mebrate <dagimcodes@gmail.com>
2024-02-29 00:47:59 +0300

    feat: added place holder error images

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -2,6 +2,23 @@
 class Assets {
   Assets._();
 
+  static const String errorsBadRequest = 'assets/images/errors/bad_request.svg';
+  static const String errorsEmptyAddress = 'assets/images/errors/empty_address.svg';
+  static const String errorsEmptyCart = 'assets/images/errors/empty_cart.svg';
+  static const String errorsEmptyList = 'assets/images/errors/empty_list.svg';
+  static const String errorsEmptyNotification = 'assets/images/errors/empty_notification.svg';
+  static const String errorsEmptyOrder = 'assets/images/errors/empty_order.svg';
+  static const String errorsEmptySearchQuery = 'assets/images/errors/empty_search_query.svg';
+  static const String errorsEmptyWalletTransaction = 'assets/images/errors/empty_wallet_transaction.svg';
+  static const String errorsForbidden = 'assets/images/errors/forbidden.svg';
+  static const String errorsGatewayTimeout = 'assets/images/errors/gateway_timeout.svg';
+  static const String errorsInternalServer = 'assets/images/errors/internal_server.svg';
+  static const String errorsNoConnection = 'assets/images/errors/no_connection.svg';
+  static const String errorsNotFound = 'assets/images/errors/not_found.svg';
+  static const String errorsServiceUnavailable = 'assets/images/errors/service_unavailable.svg';
+  static const String errorsTooManyRequests = 'assets/images/errors/too_many_requests.svg';
+  static const String errorsUnauthorized = 'assets/images/errors/unauthorized.svg';
+  static const String errorsUnknown = 'assets/images/errors/unknown.svg';
   static const String fontsPoppinsBold = 'assets/fonts/Poppins-Bold.ttf';
   static const String fontsPoppinsExtraBold = 'assets/fonts/Poppins-ExtraBold.ttf';
   static const String fontsPoppinsExtraLight = 'assets/fonts/Poppins-ExtraLight.ttf';
@@ -11,6 +28,10 @@ class Assets {
   static const String fontsPoppinsRegular = 'assets/fonts/Poppins-Regular.ttf';
   static const String fontsPoppinsSemiBold = 'assets/fonts/Poppins-SemiBold.ttf';
   static const String fontsPoppinsThin = 'assets/fonts/Poppins-Thin.ttf';
+  static const String illustrationsChangeProfile = 'assets/images/illustrations/change_profile.svg';
+  static const String illustrationsEnterVerificationCode = 'assets/images/illustrations/enter_verification_code.svg';
+  static const String illustrationsForgotPassword = 'assets/images/illustrations/forgot_password.svg';
+  static const String illustrationsResetPassword = 'assets/images/illustrations/reset_password.svg';
   static const String illustrationsTooManyRequests = 'assets/images/illustrations/too_many_requests.svg';
   static const String illustrationsWinnersPana = 'assets/images/illustrations/Winners-pana.svg';
   static const String imagesIllustrationsTooManyRequests = 'assets/images/illustrations/too_many_requests.svg';
