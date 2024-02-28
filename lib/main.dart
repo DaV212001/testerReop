c0857dc4ee8a3a590bf2728d8d89237eb11b75dc
Dagim Tesfaye Mebrate <dagimcodes@gmail.com>
2024-02-29 00:49:46 +0300

    fix: firebase init fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:fast_cached_network_image/fast_cached_network_image.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mss_e_learning/screen/splash/splash_screen.dart';
 import 'package:mss_e_learning/service/firebase_service.dart';
 import 'package:mss_e_learning/util/app_routes.dart';
+import 'package:mss_e_learning/util/firebase_options.dart';
 import 'config/config_preference.dart';
 import 'config/themes/data/app_themes.dart';
 import 'config/themes/theme_manager.dart';
@@ -20,6 +22,9 @@ Future<void> main() async {
   // init image caching
   await FastCachedImageConfig.init(clearCacheAfter: const Duration(days: 15));
   // init firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await FirebaseService().init();
 
 
