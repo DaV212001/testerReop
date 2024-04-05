cb0aac13d0d3ef6821fdbf0cbd268c75f6deb8a3
DaV212001 <129734361+DaV212001@users.noreply.github.com>
2024-04-05 17:59:17 +0300

    phpMyAdmin first upload

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class AppConstants {
   static const String api = "https://api.techsavvyadademia.com/api";
   static const String imagebaseurl = "https://api.techsavvyadademia.com/public/storage/";
-  static const int subcatid = 22;
+  static const int subcatid = 23;
   static const Color primary = Color(0xFF4B39EF);
-  static const String authStorage = 'sql_authorization_token';
-  static const String packageName = 'com.msslearn.sql';
-  static const String appName = 'SQL';
+  static const String authStorage = 'phpmdmn_authorization_token';
+  static const String packageName = 'com.msslearn.php_my_admin';
+  static const String appName = 'phpMyAdmin';
   static const String headerAppName = 'Tech Savvy';
   static var kAuthContainerGradient = const BoxDecoration(
     gradient: LinearGradient(
