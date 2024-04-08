3b83361135017b7f55e8d0c3b92d73ff1792060f
DaV212001 <129734361+DaV212001@users.noreply.github.com>
2024-04-08 18:24:46 +0300

    GD first upload

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class AppConstants {
   static const String api = "https://api.techsavvyadademia.com/api";
   static const String imagebaseurl = "https://api.techsavvyadademia.com/public/storage/";
-  static const int subcatid = 28;
+  static const int subcatid = 29;
   static const Color primary = Color(0xFF4B39EF);
-  static const String authStorage = 'css_authorization_token';
-  static const String packageName = 'com.msslearn.css';
-  static const String appName = 'CSS';
+  static const String authStorage = 'gd_authorization_token';
+  static const String packageName = 'com.msslearn.gd';
+  static const String appName = 'Game Development';
   static const String headerAppName = 'Tech Savvy';
   static var kAuthContainerGradient = const BoxDecoration(
     gradient: LinearGradient(
