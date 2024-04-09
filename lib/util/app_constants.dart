cc3f49b3b94b2dd0494c74fa9806b5658969e587
DaV212001 <129734361+DaV212001@users.noreply.github.com>
2024-04-09 19:39:19 +0300

    Ruby first upload

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class AppConstants {
   static const String api = "https://api.techsavvyadademia.com/api";
   static const String imagebaseurl = "https://api.techsavvyadademia.com/public/storage/";
-  static const int subcatid = 41;
+  static const int subcatid = 42;
   static const Color primary = Color(0xFF4B39EF);
-  static const String authStorage = 'st_authorization_token';
-  static const String packageName = 'com.msslearn.st';
-  static const String appName = 'Swift';
+  static const String authStorage = 'rb_authorization_token';
+  static const String packageName = 'com.msslearn.rb';
+  static const String appName = 'Ruby';
   static const String headerAppName = 'Tech Savvy';
   static var kAuthContainerGradient = const BoxDecoration(
     gradient: LinearGradient(
